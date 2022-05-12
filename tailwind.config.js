a26ad9ea6d9d949b5da78ce5ff8984020d35287a
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-12 12:55:22 +0600

    customize my theme from taildwinf config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,21 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0FCFEC",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "white",
+      "cupcake",
+    ],
+  },
+  
   plugins: [require("daisyui")],
 }
