94e6f54a7db3cc9f135824f79fb60af9f4f3854a
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-13 02:13:02 +0600

    use daisy Ui form on home page

--- src/Pages/Home/FormSection/FormSection.js
+++ src/Pages/Home/FormSection/FormSection.js
@@ -7,18 +7,26 @@ const FormSection = () => {
     return (
         <div className='py-12' style={{ backgroundImage: `url(${appointment})` }}>
             <SectionTitle align='text-center' title='Contact Us' subTitle='Stay connected with us' subTitleColor='text-white'></SectionTitle>
+
             <div className='flex justify-center'>
-                <form>
-                    <input className=' my-2 p-3  w-full rounded-lg' type="email" name="" placeholder='Email Address' />
-                    <br />
-                    <input className=' my-2 p-3  w-full rounded-lg' type="text" name="" placeholder='Subject' />
-                    <br />
-                    <textarea className=' my-2 p-3  w-full rounded-lg' name="" cols="30" rows="5" placeholder='Your message'></textarea>
-                    <br />
-                    <div className='text-center'>
-                        <input type="submit" value="Submit" className='btn inline-block btn-primary text-white bg-gradient-to-r from-secondary to-primary hover:from-primary hover:to-secondary' />
+                <div class="card w-full max-w-sm ">
+                    <div class="card-body">
+                        <div class="form-control">
+
+                            <input type="email" placeholder="Enter Email" class="input input-bordered" />
+                        </div>
+                        <div class="form-control">
+
+                            <input type="text" placeholder="Subject" class="input input-bordered" />
+                        </div>
+                        <div class="form-control">
+                            <textarea name="" id="" placeholder="Message" className='input h-40 input-bordered' cols="30" rows="10"></textarea>
+                        </div>
+                        <div class="form-control mt-4">
+                            <button class="btn btn-primary">Login</button>
+                        </div>
                     </div>
-                </form>
+                </div>
             </div>
 
         </div>
