3fae3d6b3b10416a5f99e6fc74e80dfb3dd99c05
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-17 13:14:19 +0600

    load service data from my own server

--- src/Pages/Appointment/AvailableServices/ServiceModal/ServiceModal.js
+++ src/Pages/Appointment/AvailableServices/ServiceModal/ServiceModal.js
@@ -5,18 +5,18 @@ const ServiceModal = ({ selectedTreatment }) => {
     const { name, slot, selectedDate } = selectedTreatment;
     return (
         <div>
-            <input type="checkbox" id="service-modal" class="modal-toggle" />
-            <div class="modal">
-                <div class="modal-box relative">
-                    <label for="service-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 class="text-lg font-bold">{name}</h3>
+            <input type="checkbox" id="service-modal" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box relative">
+                    <label htmlFor="service-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <h3 className="text-lg font-bold">{name}</h3>
 
                     <div className='mt-5'>
-                        <input type="text" disabled value={format(selectedDate, 'PP')} class="input my-2 input-bordered input-sm  w-full" />
-                        <input type="text" disabled value={slot} class="input my-2 input-sm input-bordered w-full " />
-                        <input type="text" placeholder='Enter Full Name' class="input my-2 input-sm input-bordered w-full " />
-                        <input type="phone" placeholder='Phone' class="input my-2 input-sm input-bordered w-full " />
-                        <input type="email" placeholder='Email' class="input my-2 input-sm input-bordered w-full " />
+                        <input type="text" disabled value={format(selectedDate, 'PP')} className="input my-2 input-bordered input-sm  w-full" />
+                        <input type="text" disabled value={slot} className="input my-2 input-sm input-bordered w-full " />
+                        <input type="text" placeholder='Enter Full Name' className="input my-2 input-sm input-bordered w-full " />
+                        <input type="phone" placeholder='Phone' className="input my-2 input-sm input-bordered w-full " />
+                        <input type="email" placeholder='Email' className="input my-2 input-sm input-bordered w-full " />
                         <button className='btn my-2 w-full btn-primary'>Submit</button>
                     </div>
 
--- src/Pages/Home/FormSection/FormSection.js
+++ src/Pages/Home/FormSection/FormSection.js
@@ -9,21 +9,21 @@ const FormSection = () => {
             <SectionTitle align='text-center' title='Contact Us' subTitle='Stay connected with us' subTitleColor='text-white'></SectionTitle>
 
             <div className='flex justify-center'>
-                <div class="card w-full max-w-sm ">
-                    <div class="card-body">
-                        <div class="form-control">
+                <div className="card w-full max-w-sm ">
+                    <div className="card-body">
+                        <div className="form-control">
 
-                            <input type="email" placeholder="Enter Email" class="input input-bordered" />
+                            <input type="email" placeholder="Enter Email" className="input input-bordered" />
                         </div>
-                        <div class="form-control">
+                        <div className="form-control">
 
-                            <input type="text" placeholder="Subject" class="input input-bordered" />
+                            <input type="text" placeholder="Subject" className="input input-bordered" />
                         </div>
-                        <div class="form-control">
+                        <div className="form-control">
                             <textarea name="" id="" placeholder="Message" className='input h-40 input-bordered' cols="30" rows="10"></textarea>
                         </div>
-                        <div class="form-control mt-4">
-                            <button class="btn btn-primary">Login</button>
+                        <div className="form-control mt-4">
+                            <button className="btn btn-primary">Login</button>
                         </div>
                     </div>
                 </div>
