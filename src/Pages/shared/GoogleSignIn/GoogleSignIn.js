5dc9aea3056275925553eed758dd409cb837bfef
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-20 12:36:56 +0600

    show error on google signin page

--- src/Pages/shared/GoogleSignIn/GoogleSignIn.js
+++ src/Pages/shared/GoogleSignIn/GoogleSignIn.js
@@ -3,6 +3,7 @@ import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import useToken from '../../../hooks/useToken';
+import Loading from '../Loading/Loading';
 
 const GoogleSignIn = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -10,16 +11,32 @@ const GoogleSignIn = () => {
     let location = useLocation();
     const [token] = useToken(gUser);
     let from = location.state?.from?.pathname || "/";
+
     useEffect(() => {
         // For google user
         if (token) {
             navigate(from, { replace: true });
         }
     }, [token, from, navigate])
+    let gErrorMessage;
 
+    if (gLoading) {
+        <Loading></Loading>
+
+    }
+
+    if (gError) {
+        gErrorMessage = <p className='text-red-500 mb-4'>{gError?.message}</p>
+
+    }
 
     return (
-        <button onClick={() => signInWithGoogle()} className="btn btn-outline btn-accent">Continue with google</button>
+        <div>
+            {
+                gErrorMessage && gErrorMessage
+            }
+            <button onClick={() => signInWithGoogle()} className="btn btn-outline w-full btn-accent">Continue with google</button>
+        </div>
     );
 };
 
