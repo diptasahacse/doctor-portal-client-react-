e6d027290d4aa67dce82f367ab83e4d6102a3ede
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-21 14:05:23 +0600

    able to set admin for a user

--- src/Pages/Dashboard/AllUsers/UserRow/UserRow.js
+++ src/Pages/Dashboard/AllUsers/UserRow/UserRow.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import UserAdminCreateModal from './UserActionModal/UserAdminCreateModal';
 
 const UserRow = ({ user, index }) => {
     const { email } = user;
 
     const makeAdminHandler = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: "PUT",
+            headers: { authorization: `Bearer ${localStorage.getItem('accessToken')}` }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
 
     }
     return (
         <tr>
             <th>{index + 1}</th>
             <td>{email}</td>
-            <td><label onClick={makeAdminHandler} htmlFor="my-modal" class="btn btn-primary btn-xs">Create Admin</label></td>
-            <td><button class="btn btn-error btn-xs">Remove Admin</button></td>
-            <UserAdminCreateModal></UserAdminCreateModal>
+            <td><button onClick={makeAdminHandler} disabled={user?.role} className="btn btn-primary btn-xs">Create Admin</button></td>
+            <td><button className="btn btn-error btn-xs">Remove Admin</button></td>
+
 
         </tr>
     );
